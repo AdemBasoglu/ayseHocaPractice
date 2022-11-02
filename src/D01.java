85dfa9941e24e804d66cec26e0ac36cd0063682a
Adem Basoglu <admbasoglu28@gmail.com>
2022-11-02 20:57:47 +0100

    buda oldu

--- src/D01.java
+++ src/D01.java
@@ -3,5 +3,8 @@ package day05;
 public class D01 {
     public static void main(String[] args) {
         System.out.println("github3 klasorune gectik");
+
+
+        System.out.println(" buda oldu ");
     }
 }
