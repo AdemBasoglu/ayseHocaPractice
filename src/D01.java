7de13c3ecb2f6c02f4bf21a4bfe42f78e0090826
Adem Basoglu <admbasoglu28@gmail.com>
2022-11-02 20:49:10 +0100

    deneme branchinde degisiklik yaptim

--- src/D01.java
+++ src/D01.java
@@ -3,5 +3,9 @@ package day05;
 public class D01 {
     public static void main(String[] args) {
         System.out.println("github3 klasorune gectik");
+
+        System.out.println("deneme");
+
+        System.out.println("deneme branchinde degisiklik yaptim");
     }
 }
